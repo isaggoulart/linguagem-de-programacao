47180a827634c52d7096371d3aeb0462e36b3024
isaggoulart <isabela.goulart@sptech.school>
2024-09-10 18:44:17 -0300

    Atv livraria final

--- AtividadeLivraria/src/main/java/school/sptech/LivroDao.java
+++ AtividadeLivraria/src/main/java/school/sptech/LivroDao.java
@@ -14,56 +14,79 @@ public class LivroDao {
         this.assistente = conexao.getTemplate();
     }
 
-    public Integer cadastrarLivro(LivroEntity livro){
+    public void cadastrarLivro(LivroEntity livro){
         String nome = livro.getNome();
         Double preco = livro.getPreco();
 
         String sqlInsert = "INSERT INTO livro (nome, preco) VALUES (?, ?)";
         assistente.update(sqlInsert, nome, preco);
-        return 0;
     }
 
-    public Integer exibirLivros () {
-        List<LivroEntity> query = assistente.query("select * from livros", new BeanPropertyRowMapper<>(LivroEntity.class));
+    public void exibirLivros () {
+        List<LivroEntity> query = assistente.query("SELECT * from livro", new BeanPropertyRowMapper<>(LivroEntity.class));
 
         for (LivroEntity livroDaVez : query) {
             System.out.println("ID do livro: " + livroDaVez.getId());
             System.out.println("Nome do livro: " + livroDaVez.getNome());
             System.out.println("Preço do livro: " + livroDaVez.getPreco());
-            System.out.println("---------------");
+            System.out.println("--------------------------");
         }
-        return 0;
     }
 
 
-    public Integer atualizarLivros (Integer id, String novoNome, Double novoPreco) {
-        List<LivroEntity> query = assistente.query("select * from livros", new BeanPropertyRowMapper<>(LivroEntity.class));
+    public void atualizarLivros (Integer id, String novoNome, Double novoPreco) {
+        Integer contador = 0;
+        List<LivroEntity> query = assistente.query("select * from livro", new BeanPropertyRowMapper<>(LivroEntity.class));
 
         for (LivroEntity livroDaVez : query) {
 
             if(livroDaVez.getId() == id) {
-
-                String sqlUpdate = "Update livros set nome = ?, preco = ? where id = ?";
+                contador++;
+                String sqlUpdate = "Update livro set nome = ?, preco = ? where id = ?";
                 assistente.update(sqlUpdate, novoNome, novoPreco,id);
+                System.out.println("A atualização foi realizada com sucesso!");
             }
         }
-
-        return 0;
+        if (contador == 0) {
+            System.out.println("Parece que o livro que você escolheu aida não está cadastrado :(");
+        }
     }
 
-   public Integer removerLivro(Integer id) {
-       List<LivroEntity> query = assistente.query("select * from livros", new BeanPropertyRowMapper<>(LivroEntity.class));
+
+   public void removerLivro(Integer id) {
+        Integer contador = 0;
+       List<LivroEntity> query = assistente.query("select * from livro", new BeanPropertyRowMapper<>(LivroEntity.class));
 
        for (LivroEntity livroDaVez : query) {
            if (livroDaVez.getId() == id) {
+               contador++;
                String sqlDelete = "Delete from livro where id = ?";
                assistente.update(sqlDelete, id);
+               System.out.println("O livro foi removido com sucesso!");
            }
        }
-       return 0;
+       if (contador == 0) {
+           System.out.println("Ops! Parece que você quer excluir um livro que não está cadastrado");
+       }
    }
 
 
+   public void buscarPorNome(String nome) {
+       Integer contador = 0;
+       List<LivroEntity> query = assistente.query("select * from livro", new BeanPropertyRowMapper<>(LivroEntity.class));
 
+       for (LivroEntity livroDaVez : query) {
+           if ((livroDaVez.getNome()).equalsIgnoreCase(nome)) {
+               contador ++;
+               System.out.println("Aqui estão as informações do livro escolhido: ");
+               System.out.println("ID do livro: " + livroDaVez.getId());
+               System.out.println("Nome do livro: " + livroDaVez.getNome());
+               System.out.println("Preço do livro: " + livroDaVez.getPreco());
+           }
+       }
+       if (contador == 0) {
+           System.out.println("Parece que este livro ainda não está cadastrado");
+       }
+   }
 
 }
--- AtividadeLivraria/src/main/java/school/sptech/Main.java
+++ AtividadeLivraria/src/main/java/school/sptech/Main.java
@@ -3,6 +3,9 @@ package school.sptech;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,12 +13,69 @@ public class Main {
         JdbcTemplate assistente = conexao.getTemplate();
 
         String sqlCreate = """
-            CREATE TABLE IF NOT EXISTS livro (
-                id INT PRIMARY KEY AUTO_INCREMENT,
-                nome VARCHAR(200),
-                preco float
-            )
-            """;
+                CREATE TABLE IF NOT EXISTS livro (
+                    id INT PRIMARY KEY AUTO_INCREMENT,
+                    nome VARCHAR(200),
+                    preco float
+                )
+                """;
         assistente.execute(sqlCreate);
+
+        Scanner texto = new Scanner(System.in);
+        Scanner numero = new Scanner(System.in);
+        LivroDao metodos = new LivroDao();
+
+        for(;;) {
+            System.out.println("+--------------------+");
+            System.out.println("| Livraria SPTech    |");
+            System.out.println("+--------------------+");
+            System.out.println("| 1) Cadastrar livro |");
+            System.out.println("| 2) Exibir livro    |");
+            System.out.println("| 3) Atualizar livro |");
+            System.out.println("| 4) Remover livro   |");
+            System.out.println("| 5) Buscar por nome |");
+            System.out.println("| 6) Sair            |");
+            System.out.println("+--------------------+");
+
+            Integer opcao = numero.nextInt();
+
+            if (opcao == 1) {
+                System.out.println("Digite o nome do livro a ser cadastrado:");
+                String nome = texto.nextLine();
+                System.out.println("Digite o preço do livro a ser cadastrado:");
+                Double preco = numero.nextDouble();
+                numero.nextLine();
+
+                LivroEntity livro = new LivroEntity(nome, preco);
+                metodos.cadastrarLivro(livro);
+            } else if (opcao == 2) {
+                System.out.println("Aqui estão os livros cadastrados: ");
+                System.out.println("----------------------------------");
+                metodos.exibirLivros();
+            } else if (opcao == 3) {
+                metodos.exibirLivros();
+                System.out.println("Escolha o id do livro que você deseja alterar:");
+                Integer id = numero.nextInt();
+                System.out.println("Digite o novo nome:");
+                String novoNome = texto.nextLine();
+                System.out.println("Digite o novo preço:");
+                Double novoPreco = numero.nextDouble();
+                numero.nextLine();
+                metodos.atualizarLivros(id, novoNome, novoPreco);
+            } else if (opcao == 4) {
+                metodos.exibirLivros();
+                System.out.println("Escolha o id do livro que você deseja remover:");
+                Integer id = numero.nextInt();
+                metodos.removerLivro(id);
+            } else if (opcao == 5) {
+                System.out.println("Digite o nome do livro que quer encontrar:");
+                String nome = texto.nextLine();
+                metodos.buscarPorNome(nome);
+            } else if (opcao == 6) {
+                break;
+            } else {
+                System.out.println("Por favor, escolha uma opção válida!");
+            }
+        }
     }
 }
